f8e217aa9b50ec37094e0490124bd11dc04e9b52
liliaungurean <lilicikau95@gmail.com>
2023-01-17 22:30:01 -0600

    added code to create new note

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+// Import express
 const express = require ('express');
 const app = express ();
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 const fs = require ('fs');
 const path = require ('path');
+
+const allNotes = require('./db/db.json')
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -18,4 +21,35 @@ app.get ('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-app.get()
+app.get('/notes', (req, res) =>{
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+// Fallback route
+app.get('*', (req, red) =>{
+    res.sendFile(path.join(__dirname, './public/index.html'));
+}); 
+
+//create new note
+function createNewNote(body, notesArray){
+    const newNote = body;
+    if (!Array.isArray(notesArray))
+        notesArray = [];
+
+    if (notesArray.length === 0)
+        notesArray.push(0);
+
+    body.id = notesArray[0];
+        notesArray[0]++;
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/bd.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNote;
+}
+
+//tell server to listen
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT}`)
+);
